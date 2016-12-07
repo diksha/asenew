b75f00fb040c7274a3ea7bf4ae9790773cfcca2e
Diksha <dgohlya@ncsu.edu>
2016-12-07 03:54:28 +0000

    New files added

--- 10/global_variables.py
+++
@@ -1,8 +0,0 @@
-
-class global_variable(object):
-    def __init__(self, model):
-        self.final_energy=[]
-        self.rdiv=[]
-        self.loss_inter=0.0
-        self.best_energy= 0
-        self.pop_front_best=[]
